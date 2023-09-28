5773ce06cd129d95128c545a62629b993be706a3
abdelilleh <143023531+Moncif2005@users.noreply.github.com>
2023-09-28 12:42:58 +0100

    Add files via upload

---
+++ gymtime.js
@@ -0,0 +1,7 @@
+const humburugr = document.querySelector(".humburugr");
+humburugr.addEventListener("click",()=>{
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("active")
+})
+
+
